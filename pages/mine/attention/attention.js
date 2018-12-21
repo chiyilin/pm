d8a54e8ad5b4f0f42f027137061d66c7556a1308
chiyilin <cyl19980205@gmail.com>
2018-12-21 09:21:59 +0800

    12.21

--- pages/mine/attention/attention.js
+++ pages/mine/attention/attention.js
@@ -1,66 +1,120 @@
 // pages/mine/attention/attention.js
+var app = getApp();
+var common = require('../../../utils/common.js');
+var request = function(that, hidd = false) {
+  console.log(that.data.start)
+  wx.showNavigationBarLoading();
+  common.Post('user/collection', {
+    user_id: that.data.userinfo.user_id,
+    limit: [that.data.start, 8]
+  }, function(res) {
+    var data = that.data.data;
+    that.setData({
+      data: data.concat(res)
+    })
+    if (hidd) {
+      wx.stopPullDownRefresh();
+    }
+    wx.hideNavigationBarLoading();
+  });
+}
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    filepath: app.globalData.filepath,
+    start: 1,
+    data: [],
   },
-
+  toupper: function() {
+    var that = this;
+    wx.startPullDownRefresh({
+      success: function() {
+        setTimeout(function() {
+          that.setData({
+            data: [],
+            start: 1
+          });
+          request(that, true);
+        }, 500);
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
+    var that = common.that = this;
+    // common.style();
+    common.globalData = app.globalData;
+    if (!common.checkAuthLogin(true)) {
+      common.login();
+    }
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          system: res
+        })
+      },
+    })
+    that.data.userinfo = common.getUserInfo();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
+    var that = this;
+
+    request(that);
 
   },
-
+  tolower: function() {
+    var that = this;
+    that.data.start++;
+    request(that)
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,4 +1,19 @@
 // pages/mine/mine.js
+var app = getApp();
+var common = require('../../utils/common.js');
+var request = function(that, hidd) {
+  common.Post('user/userinfo', {
+    user_id: that.data.userinfo.user_id
+  }, function(data) {
+    that.setData({
+      userinfo: data
+    })
+    wx.setStorageSync('userinfo', JSON.stringify(data))
+    if (hidd) {
+      wx.stopPullDownRefresh();
+    }
+  });
+}
 Page({
 
   /**
@@ -11,74 +26,84 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    var that = common.that = this;
+    // common.style();
+    common.globalData = app.globalData;
+    if (!common.checkAuthLogin(true)) {
+      common.login();
+    }
+    that.setData({
+      userinfo: common.getUserInfo()
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    var that = this;
+    request(that);
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
+  onPullDownRefresh: function() {
+    var that = this;
+    request(that, true);
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
- pay: function () {
+  pay: function() {
     wx.navigateTo({
       url: "paid/paid",
     })
   },
-  attention: function () {
+  attention: function() {
     wx.navigateTo({
       url: "attention/attention",
     })
   },
-  card: function () {
+  card: function() {
     wx.navigateTo({
       url: "card/card",
     })
   },
-  note: function () {
+  note: function() {
     wx.navigateTo({
       url: "TransactionRecords/TransactionRecords",
     })
